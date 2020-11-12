b1b139861feb291e84c0ba0825962b14f6eabaca
Airenas Vaičiūnas <airenass@gmail.com>
2020-11-12 12:08:01 +0200

    Add URL test

--- internal/pkg/processor/utils.go
+++ internal/pkg/processor/utils.go
@@ -2,11 +2,15 @@ package processor
 
 import (
 	"net/url"
+	"strings"
 
 	"github.com/pkg/errors"
 )
 
 func checkURL(urlStr string) (string, error) {
+	if strings.TrimSpace(urlStr) == "" {
+		return "", errors.New("No url")
+	}
 	urlRes, err := url.Parse(urlStr)
 	if err != nil {
 		return "", errors.Wrap(err, "Can't parse url "+urlStr)
---
+++ internal/pkg/processor/utils_test.go
@@ -0,0 +1,20 @@
+package processor
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestURL(t *testing.T) {
+	_, err := checkURL("")
+	assert.NotNil(t, err)
+	_, err = checkURL(":8080")
+	assert.NotNil(t, err)
+
+	res, err := checkURL("http://local:8080")
+	assert.Nil(t, err)
+
+	assert.Equal(t, "http://local:8080", res)
+
+}
