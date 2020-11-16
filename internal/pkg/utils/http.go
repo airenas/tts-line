cdbec554eb08fbcc152eecdda61490e8698c02b8
Airenas Vaičiūnas <airenass@gmail.com>
2020-11-16 09:25:21 +0200

    Add more logging

--- internal/pkg/utils/http.go
+++ internal/pkg/utils/http.go
@@ -23,7 +23,7 @@ func NewHTTWrap(urlStr string) (*HTTPWrap, error) {
 	var err error
 	res.URL, err = checkURL(urlStr)
 	if err != nil {
-		return nil, errors.Wrap(err, "Can't parse url")
+		return nil, errors.Wrapf(err, "Can't parse url '%s'", urlStr)
 	}
 	res.HTTPClient = &http.Client{}
 	res.flog = func(st, data string) { LogData(st, data) }
@@ -34,7 +34,7 @@ func NewHTTWrap(urlStr string) (*HTTPWrap, error) {
 func (hw *HTTPWrap) InvokeText(dataIn string, dataOut interface{}) error {
 	req, err := http.NewRequest("POST", hw.URL, strings.NewReader(dataIn))
 	if err != nil {
-		return err
+		return errors.Wrapf(err, "Can't prepare request to '%s'", hw.URL)
 	}
 	hw.flog("Input : ", dataIn)
 	req.Header.Set("Content-Type", "text/plain")
@@ -51,7 +51,7 @@ func (hw *HTTPWrap) InvokeJSON(dataIn interface{}, dataOut interface{}) error {
 	hw.flog("Input : ", b.String())
 	req, err := http.NewRequest("POST", hw.URL, b)
 	if err != nil {
-		return err
+		return errors.Wrapf(err, "Can't prepare request to '%s'", hw.URL)
 	}
 	req.Header.Set("Content-Type", "application/json")
 	return hw.invoke(req, dataOut)
@@ -61,11 +61,11 @@ func (hw *HTTPWrap) invoke(req *http.Request, dataOut interface{}) error {
 	hw.flog("Call : ", hw.URL)
 	resp, err := hw.HTTPClient.Do(req)
 	if err != nil {
-		return err
+		return errors.Wrapf(err, "Can't call '%s'", hw.URL)
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode < 200 || resp.StatusCode > 299 {
-		return errors.New("Can't invoke")
+		return errors.Errorf("Can't invoke '%s'. Code: '%d'", hw.URL, resp.StatusCode)
 	}
 	br, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
