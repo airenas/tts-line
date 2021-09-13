efcec4b7d53e8fc4d1f3b4c574d63bb479fb0049
Airenas Vaičiūnas <airenass@gmail.com>
2021-09-13 18:42:38 +0300

    Add banner to am-voc wrapper

--- cmd/am-voc-wrapper/main.go
+++ cmd/am-voc-wrapper/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/airenas/go-app/pkg/goapp"
 	"github.com/airenas/tts-line/internal/pkg/wrapservice"
+	"github.com/labstack/gommon/color"
 
 	"github.com/pkg/errors"
 )
@@ -18,8 +19,36 @@ func main() {
 		goapp.Log.Fatal(errors.Wrap(err, "Can't init processor"))
 	}
 
+	printBanner()
+
 	err = wrapservice.StartWebServer(&data)
 	if err != nil {
 		goapp.Log.Fatal(errors.Wrap(err, "Can't start the service"))
 	}
+}
+
+var (
+	version string
+)
+
+func printBanner() {
+	banner := `
+    ___    __  ___            _    ______  ______
+   /   |  /  |/  /           | |  / / __ \/ ____/
+  / /| | / /|_/ /  ______    | | / / / / / /     
+ / ___ |/ /  / /  /_____/    | |/ / /_/ / /___   
+/_/  |_/_/  /_/              |___/\____/\____/   
+                                                 
+ _       ______  ___    ____  ____  __________ 
+| |     / / __ \/   |  / __ \/ __ \/ ____/ __ \
+| | /| / / /_/ / /| | / /_/ / /_/ / __/ / /_/ /
+| |/ |/ / _, _/ ___ |/ ____/ ____/ /___/ _, _/ 
+|__/|__/_/ |_/_/  |_/_/   /_/   /_____/_/ |_|   v: %s 
+
+%s
+________________________________________________________                                                 
+
+`
+	cl := color.New()
+	cl.Printf(banner, cl.Red(version), cl.Green("https://github.com/airenas/tts-line"))
 }
