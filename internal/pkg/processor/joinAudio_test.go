442488c94f7afa28abcaa84aba1fa4960230c879
Airenas Vaičiūnas <airenass@gmail.com>
2022-05-20 11:36:06 +0300

    Add join audio tests

--- internal/pkg/processor/joinAudio_test.go
+++ internal/pkg/processor/joinAudio_test.go
@@ -1,16 +1,17 @@
 package processor
 
 import (
+	"bytes"
 	"encoding/base64"
 	"io/ioutil"
 	"testing"
+	"time"
 
 	"github.com/airenas/tts-line/internal/pkg/service/api"
-	"github.com/airenas/tts-line/internal/pkg/wav"
-
-	"github.com/stretchr/testify/assert"
-
 	"github.com/airenas/tts-line/internal/pkg/synthesizer"
+	"github.com/airenas/tts-line/internal/pkg/wav"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestNewJoinAudio(t *testing.T) {
@@ -54,7 +55,7 @@ func TestJoinAudio_Several(t *testing.T) {
 	as := getTestAudioSize(strA)
 
 	assert.Equal(t, as*3, getTestAudioSize(d.Audio))
-	assert.InDelta(t, 0.5572 * 3, d.AudioLenSeconds, 0.001)
+	assert.InDelta(t, 0.5572*3, d.AudioLenSeconds, 0.001)
 }
 
 func TestJoinAudio_DecodeFail(t *testing.T) {
@@ -77,6 +78,114 @@ func TestJoinAudio_EmptyFail(t *testing.T) {
 	assert.NotNil(t, err)
 }
 
+func TestJoinSSMLAudio(t *testing.T) {
+	pr := NewJoinSSMLAudio()
+	d := synthesizer.TTSData{Input: &api.TTSRequestConfig{OutputFormat: api.AudioMP3}}
+	strA := getTestEncAudio(t)
+	d.Parts = []*synthesizer.TTSDataPart{{Audio: strA}}
+	d.Cfg.Type = synthesizer.SSMLText
+	da := &synthesizer.TTSData{Input: d.Input, SSMLParts: []*synthesizer.TTSData{&d}}
+	err := pr.Process(da)
+	assert.Nil(t, err)
+	assert.Equal(t, strA, da.Audio)
+	assert.InDelta(t, 0.5572, da.AudioLenSeconds, 0.001)
+}
+
+func TestJoinSSMLAudio_Skip(t *testing.T) {
+	pr := NewJoinSSMLAudio()
+	d := synthesizer.TTSData{Input: &api.TTSRequestConfig{OutputFormat: api.AudioNone}}
+	strA := getTestEncAudio(t)
+	d.Parts = []*synthesizer.TTSDataPart{{Audio: strA}}
+	d.Cfg.Type = synthesizer.SSMLText
+	da := &synthesizer.TTSData{Input: d.Input, SSMLParts: []*synthesizer.TTSData{&d}}
+	err := pr.Process(da)
+	assert.Nil(t, err)
+	assert.Equal(t, "", da.Audio)
+	assert.InDelta(t, 0.0, da.AudioLenSeconds, 0.001)
+}
+
+func TestJoinSSMLAudio_Several(t *testing.T) {
+	initTestJSON(t)
+	pr := NewJoinSSMLAudio()
+	d := synthesizer.TTSData{Input: &api.TTSRequestConfig{OutputFormat: api.AudioMP3}}
+	strA := getTestEncAudio(t)
+	d.Parts = []*synthesizer.TTSDataPart{{Audio: strA}, {Audio: strA},
+		{Audio: strA}}
+	d.Cfg.Type = synthesizer.SSMLText
+	da := &synthesizer.TTSData{Input: d.Input, SSMLParts: []*synthesizer.TTSData{&d, &d}}
+	err := pr.Process(da)
+	assert.Nil(t, err)
+
+	as := getTestAudioSize(strA)
+
+	assert.Equal(t, as*6, getTestAudioSize(da.Audio))
+	assert.InDelta(t, 0.5572*6, da.AudioLenSeconds, 0.001)
+}
+
+func TestJoinSSMLAudio_DecodeFail(t *testing.T) {
+	initTestJSON(t)
+	pr := NewJoinSSMLAudio()
+	d := synthesizer.TTSData{Input: &api.TTSRequestConfig{OutputFormat: api.AudioMP3}}
+	strA := getTestEncAudio(t)
+	d.Parts = []*synthesizer.TTSDataPart{{Audio: strA}, {Audio: "aaa"}}
+	da := &synthesizer.TTSData{Input: d.Input, SSMLParts: []*synthesizer.TTSData{&d}}
+	err := pr.Process(da)
+	assert.NotNil(t, err)
+}
+
+func TestJoinSSMLAudio_EmptyFail(t *testing.T) {
+	initTestJSON(t)
+	pr := NewJoinSSMLAudio()
+	d := synthesizer.TTSData{Input: &api.TTSRequestConfig{OutputFormat: api.AudioMP3}}
+	strA := getTestEncAudio(t)
+	d.Parts = []*synthesizer.TTSDataPart{{Audio: strA}, {Audio: ""}}
+	da := &synthesizer.TTSData{Input: d.Input, SSMLParts: []*synthesizer.TTSData{&d}}
+	err := pr.Process(da)
+	assert.NotNil(t, err)
+}
+
+func TestJoinSSMLAudio_AddPause(t *testing.T) {
+	pr := NewJoinSSMLAudio()
+	d := &synthesizer.TTSData{Input: &api.TTSRequestConfig{OutputFormat: api.AudioMP3}}
+	strA := getTestEncAudio(t)
+	d.Parts = []*synthesizer.TTSDataPart{{Audio: strA}}
+	d.Cfg.Type = synthesizer.SSMLText
+	dp := &synthesizer.TTSData{}
+	dp.Cfg.Type = synthesizer.SSMLPause
+	dp.Cfg.PauseDuration = time.Second * 5
+
+	as := getTestAudioSize(strA)
+	ps := getTestPauseSize(strA, dp.Cfg.PauseDuration)
+	al := 0.5572
+
+	tests := []struct {
+		name     string
+		args     []*synthesizer.TTSData
+		wantSize uint32
+		wantLen  float64
+		wantErr  bool
+	}{
+		{name: "simple", args: []*synthesizer.TTSData{d}, wantSize: as, wantLen: al, wantErr: false},
+		{name: "pause start", args: []*synthesizer.TTSData{dp, d}, wantSize: as + ps, wantLen: al + 5, wantErr: false},
+		{name: "pause end", args: []*synthesizer.TTSData{d, dp}, wantSize: as + ps, wantLen: al + 5, wantErr: false},
+		{name: "pause middle", args: []*synthesizer.TTSData{d, dp, d}, wantSize: as*2 + ps, wantLen: al*2 + 5, wantErr: false},
+		{name: "max 10 sec", args: []*synthesizer.TTSData{d, dp, dp, dp}, wantSize: as + ps*2, wantLen: al + 10, wantErr: false},
+		{name: "just pause", args: []*synthesizer.TTSData{dp, dp, dp}, wantSize: 0, wantLen: 0, wantErr: true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			da := &synthesizer.TTSData{Input: d.Input, SSMLParts: tt.args}
+			err := pr.Process(da)
+			require.Equal(t, tt.wantErr, err != nil)
+			if !tt.wantErr {
+				assert.Equal(t, tt.wantSize, getTestAudioSize(da.Audio))
+				assert.InDelta(t, tt.wantLen, da.AudioLenSeconds, 0.001)
+			}
+		})
+	}
+
+}
+
 func getTestEncAudio(t *testing.T) string {
 	return base64.StdEncoding.EncodeToString(getWaveData(t))
 }
@@ -86,8 +195,46 @@ func getTestAudioSize(as string) uint32 {
 	return wav.GetSize(bt)
 }
 
+func getTestPauseSize(as string, dur time.Duration) uint32 {
+	bt, _ := base64.StdEncoding.DecodeString(as)
+	sr := wav.GetSampleRate(bt)
+	bps := wav.GetBitsPerSample(bt)
+	return uint32(dur.Milliseconds() * int64(sr) * int64(bps) / (8 * 1000))
+}
+
 func getWaveData(t *testing.T) []byte {
 	res, err := ioutil.ReadFile("../wav/_testdata/test.wav")
 	assert.Nil(t, err)
 	return res
+}
+
+func Test_writePause(t *testing.T) {
+	type args struct {
+		sampleRate    uint32
+		bitsPerSample uint16
+		pause         time.Duration
+	}
+	tests := []struct {
+		name string
+		args args
+		want uint32
+	}{
+		{name: "simple", args: args{sampleRate: 22050, bitsPerSample: 16, pause: time.Second}, want: 22050 * 2},
+		{name: "8 bits", args: args{sampleRate: 22050, bitsPerSample: 8, pause: time.Second}, want: 22050},
+		{name: "24 bits", args: args{sampleRate: 22050, bitsPerSample: 24, pause: time.Second}, want: 22050 * 3},
+		{name: "9s", args: args{sampleRate: 22050, bitsPerSample: 16, pause: time.Second * 9}, want: 22050 * 2 * 9},
+		{name: "15s", args: args{sampleRate: 22050, bitsPerSample: 16, pause: time.Second * 15}, want: 22050 * 2 * 10},
+		{name: "-1s", args: args{sampleRate: 22050, bitsPerSample: 16, pause: - time.Second * 9}, want: 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			buf := bytes.Buffer{}
+			got, _ := writePause(&buf, tt.args.sampleRate, tt.args.bitsPerSample, tt.args.pause)
+			assert.Equal(t, tt.want, got)
+			require.Equal(t, int(tt.want), len(buf.Bytes()))
+			for _, b := range buf.Bytes() {
+				require.Equal(t, byte(0), b)
+			}
+		})
+	}
 }
