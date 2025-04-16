45d87dbb6b8b20ed1418fc6d85b2a6b1de7366e7
Airenas Vaiciunas <airenass@gmail.com>
2025-04-16 18:07:07 +0300

    feat: implement grpcMetadataCarrier for gRPC metadata handling

--- internal/pkg/utils/trace.go
+++ internal/pkg/utils/trace.go
@@ -2,9 +2,9 @@ package utils
 
 import (
 	"context"
+	"strings"
 
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/trace"
 	"google.golang.org/grpc/metadata"
 )
@@ -15,6 +15,29 @@ func StartSpan(ctx context.Context, name string, opts ...trace.SpanStartOption) 
 
 func InjectTraceToGRPC(ctx context.Context) context.Context {
 	md := metadata.New(nil)
-	otel.GetTextMapPropagator().Inject(ctx, propagation.HeaderCarrier(md))
+	otel.GetTextMapPropagator().Inject(ctx, grpcMetadataCarrier(md))
 	return metadata.NewOutgoingContext(ctx, md)
+}
+
+type grpcMetadataCarrier metadata.MD
+
+func (c grpcMetadataCarrier) Get(key string) string {
+	values := metadata.MD(c).Get(strings.ToLower(key))
+	if len(values) == 0 {
+		return ""
+	}
+	return values[0]
+}
+
+func (c grpcMetadataCarrier) Set(key string, value string) {
+	lowerKey := strings.ToLower(key) // gRPC metadata keys are required to be lowercase
+	metadata.MD(c).Set(lowerKey, value)
+}
+
+func (c grpcMetadataCarrier) Keys() []string {
+	var keys []string
+	for k := range metadata.MD(c) {
+		keys = append(keys, k)
+	}
+	return keys
 }
