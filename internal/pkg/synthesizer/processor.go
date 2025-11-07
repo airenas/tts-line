cc9dba4b8130624711d969f41dbef001c01d97ad
Airenas Vaiciunas <airenass@gmail.com>
2025-11-07 15:33:31 +0200

    fix: lint

--- internal/pkg/synthesizer/processor.go
+++ internal/pkg/synthesizer/processor.go
@@ -35,7 +35,7 @@ func (mw *MainWorker) Work(ctx context.Context, input *api.TTSRequestConfig) (*a
 	data.Input = input
 	data.Cfg.Input = input
 	data.Cfg.Type = SSMLNone
-	data.Cfg.Prosodies = []*ssml.Prosody{&ssml.Prosody{Rate: input.Speed}}
+	data.Cfg.Prosodies = []*ssml.Prosody{{Rate: input.Speed}}
 	data.Cfg.Voice = input.Voice
 	data.RequestID = input.RequestID
 	data.AudioSuffix = input.AudioSuffix
